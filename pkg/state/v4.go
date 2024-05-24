a7fda947c71e46291e8f7ea66da0be7df40202cd
Colin Douglas <colin@chainguard.dev>
2024-05-24 10:15:05 -0700

    refactor and add provider flag
    Signed-off-by: Colin Douglas <colin@chainguard.dev>

---
+++ pkg/state/v4.go
@@ -0,0 +1,37 @@
+package state
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type V4 struct {
+	Resources []Resource
+	Version   int
+}
+
+type Resource struct {
+	Mode      string
+	Type      string
+	Name      string
+	Provider  string
+	Instances []Instance
+}
+
+type Instance struct {
+	Attributes   map[string]interface{}
+	Dependencies []string
+	IndexKey     interface{} `json:"index_key"`
+}
+
+func ParseStateFile(filename string) (V4, error) {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		return V4{}, err
+	}
+
+	var s V4
+	err = json.Unmarshal(bs, &s)
+
+	return s, err
+}
